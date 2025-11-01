22ac1f748ea4b615dea04f4404ac97f21390595e
Maryam Rezaee <ms.maryamrezaee@gmail.com>
2025-11-01 15:22:02 +0330

    chore(scripts): Add scripts package initializer (scripts/__init__.py)

---
+++ scripts/__init__.py
@@ -0,0 +1,10 @@
+"""
+This package contains runnable scripts for training models and debugging.
+
+Scripts:
+- check_gradients.py: Numerically verifies the gradients of all layers.
+- train_mnist.py: Trains a classifier on the MNIST (digits) dataset.
+- train_fashion_mnist.py: Trains a classifier on the Fashion-MNIST dataset.
+- train_regression.py: Trains a regressor on the California Housing dataset.
+"""
+pass
