fbf32b16a976d88d1c3e742aa3184e1356e4ce15
Maryam Rezaee <ms.maryamrezaee@gmail.com>
2025-11-01 14:59:37 +0330

    chore(utils): Add utils package initializer (src/utils/__init__.py)

---
+++ src/utils/__init__.py
@@ -0,0 +1,7 @@
+"""
+This package contains utility functions for the neural network library, including:
+- data_utils: Helpers for loading and processing datasets.
+- gradient_check: Numerical gradient checking for debugging.
+- logger: Centralized logging setup.
+"""
+pass
